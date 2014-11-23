21b7311a7c3faddb4fcde2806284ecad35b03b6e
Jeff Eldridge <eldridge.jeff@gmail.com>
2014-11-23 22:52:30 +0000

    FEATURE: Smooth landing page image transitions

--- js/CoverController.js
+++ js/CoverController.js
@@ -5,48 +5,29 @@ var coverControllers = angular.module('CoverControllers', []);
 coverControllers.controller('CoverController', ['$scope', '$timeout',
     function ($scope, $timeout) {
 
-        var
-            landscapes = [
-                "Close Up.jpg",
-                "Final 1.jpg",
-                "Final 3.jpg",
-                "Group Blurred.jpg"
-                // "PopArt1.jpg"
-            ],
-
-            portraits = [
-                "Fed1.jpg",
-                "Portrait-1-Blurred.jpg",
-                "PopArtOffsetPortrait.jpg"
-            ],
-
-            nb = 0,
-            delay = 10000,
-            cycleBackground = function () {
-                nb++;
-                $scope.Landscape = landscapes[nb % landscapes.length];
-                $scope.Portrait = portraits[nb % portraits.length];
-                $timeout (cycleBackground, delay);
-            };
-
-        $scope.bg = function () {
-            return nb % 2 === 0;
-        };
-
-        landscapes = _.shuffle(landscapes);
-        landscapes = _.shuffle(landscapes);
-        landscapes = _.shuffle(landscapes);
-
-        $scope.Landscape = landscapes[0];
-        $scope.Portrait = portraits[0];
-
-        $timeout (cycleBackground, delay);
-
         $scope.Availability = "mid-August 2014";
 
         $scope.Name = "Jeff Eldridge";
         $scope.Email = "jeff@rathmoreit.com";
         $scope.PhoneNumber = "+353 87 916 2652";
+
+        var backgroundClasses = ['bg1', 'bg2', 'bg3', 'bg4'],
+            bi = _.random(0, (backgroundClasses.length - 1)),
+            bgt = 6000;
+
+        var cycleBackground = function () {
+
+            $('html').removeClass(backgroundClasses[bi]);
+            bi++;
+            bi = bi % backgroundClasses.length;
+            $('html').addClass(backgroundClasses[bi]);
+
+            $timeout(cycleBackground, bgt);
+
+        };
+
+        cycleBackground();
+
 
     }
 ]);
